dee9b73481213e49caee8302d716e1d6b51a6a21
Noelle Laureano <nrlaureano@gmail.com>
2020-03-17 10:42:50 -0400

    sets starting point

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const fetchResponse = async () => {
-
-  console.log('Hola! Running fetchResponse!')
-
-  const backendResponse = await fetch('http://localhost:1337/api')
-  const data = await backendResponse.json()
-
-  console.log('backendResponse -->', data)
-
-  const parentDiv = document.getElementById('parent')
-
-  const childDiv = document.createElement('div')
-
-  childDiv.append(`Backend responded with: ${data}`)
-
-  parentDiv.append(childDiv)
-}
-
-fetchResponse()
+// TODO:
+// Make a fetch request to the backend server
+// GET http://localhost:1337/api
