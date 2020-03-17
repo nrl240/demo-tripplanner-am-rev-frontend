49561408bf3ddd367c40cdd6b7cd56c47e78f833
Noelle Laureano <nrlaureano@gmail.com>
2020-03-17 11:41:40 -0400

    adds try/catch

--- index.js
+++ index.js
@@ -3,21 +3,22 @@
 // GET http://localhost:1337/api
 
 const fetchResponse = async () => {
-
   console.log('Hola! Running fetchResponse!')
 
-  const backendResponse = await fetch('http://localhost:1337/api')
-  const data = await backendResponse.json()
+  try {
+    // Send request to backend server
+    const backendResponse = await fetch('http://localhost:1337/api')
+    const data = await backendResponse.json()
+    console.log('backendResponse -->', data)
 
-  console.log('backendResponse -->', data)
-
-  const parentDiv = document.getElementById('parent')
-
-  const childDiv = document.createElement('div')
-
-  childDiv.append(`Backend responded with: ${data}`)
-
-  parentDiv.append(childDiv)
+    // Create div with response data and append to parent div
+    const childDiv = document.createElement('div')
+    childDiv.append(`Backend responded with: ${data}`)
+    const parentDiv = document.getElementById('parent')
+    parentDiv.append(childDiv)
+  } catch(err) {
+    console.err(`Error retrieving data from backend server: ${err}`)
+  }
 }
 
 fetchResponse()
